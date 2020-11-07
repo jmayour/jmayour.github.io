75dffa22bb466fb4afe3fed728c2bda7420f7f6e
jmayour <jmayourian@gmail.com>
2020-11-06 20:26:46 -0500

    Update path.php

--- BCRP2019/path.php
+++ BCRP2019/path.php
@@ -1,5 +1,5 @@
 <?php
-   $path = getcwd();
-   echo "Your Absoluthe Path is: ";
-   echo $path;
-?>
+    $dir = dirname(__FILE__);
+    echo "<p>Full path to this dir: " . $dir . "</p>";
+    echo "<p>Full path to a .htpasswd file in this dir: " . $dir . "/.htpasswd" . "</p>";
+    ?>
